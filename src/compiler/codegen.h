9bfd193d486116ab75b4ad17c1d8ac19d0f70126
Rani367 <rani2011367@gmail.com>
2025-08-06 12:50:45 +0300

    Add engine core

---
+++ src/compiler/codegen.h
@@ -0,0 +1,8 @@
+
+// Get an error by index
+Error* codegen_get_error(Codegen* codegen, int index);
+
+// Print all errors
+void codegen_print_errors(Codegen* codegen, FILE* out);
+
+#endif // PH_CODEGEN_H
