d38a12a1117b80857375553da3d4e1ef98773848
Rani367 <rani2011367@gmail.com>
2025-08-18 09:08:51 +0300

    Implement tokenizer

---
+++ src/main.c
@@ -0,0 +1,101 @@
+;
+    analyzer_declare_global(analyzer, "random_int");
+    analyzer_declare_global(analyzer, "len");
+    analyzer_declare_global(analyzer, "push");
+    analyzer_declare_global(analyzer, "pop");
+    analyzer_declare_global(analyzer, "insert");
+    analyzer_declare_global(analyzer, "remove");
+    analyzer_declare_global(analyzer, "contains");
+    analyzer_declare_global(analyzer, "index_of");
+    analyzer_declare_global(analyzer, "substring");
+    analyzer_declare_global(analyzer, "split");
+    analyzer_declare_global(analyzer, "join");
+    analyzer_declare_global(analyzer, "upper");
+    analyzer_declare_global(analyzer, "lower");
+    analyzer_declare_global(analyzer, "range");
+    analyzer_declare_global(analyzer, "time");
+    analyzer_declare_global(analyzer, "clock");
+    analyzer_declare_global(analyzer, "vec2");
+
+    // Engine functions
+    analyzer_declare_global(analyzer, "rgb");
+    analyzer_declare_global(analyzer, "rgba");
+    analyzer_declare_global(analyzer, "create_window");
+    analyzer_declare_global(analyzer, "set_title");
+    analyzer_declare_global(analyzer, "window_width");
+    analyzer_declare_global(analyzer, "window_height");
+    analyzer_declare_global(analyzer, "clear");
+    analyzer_declare_global(analyzer, "draw_rect");
+    analyzer_declare_global(analyzer, "draw_circle");
+    analyzer_declare_global(analyzer, "draw_line");
+    analyzer_declare_global(analyzer, "key_down");
+    analyzer_declare_global(analyzer, "key_pressed");
+    analyzer_declare_global(analyzer, "mouse_x");
+    analyzer_declare_global(analyzer, "mouse_y");
+    analyzer_declare_global(analyzer, "mouse_down");
+    analyzer_declare_global(analyzer, "delta_time");
+    analyzer_declare_global(analyzer, "game_time");
+
+    // Image and sprite functions
+    analyzer_declare_global(analyzer, "load_image");
+    analyzer_declare_global(analyzer, "image_width");
+    analyzer_declare_global(analyzer, "image_height");
+    analyzer_declare_global(analyzer, "draw_image");
+    analyzer_declare_global(analyzer, "draw_image_ex");
+    analyzer_declare_global(analyzer, "create_sprite");
+    analyzer_declare_global(analyzer, "draw_sprite");
+    analyzer_declare_global(analyzer, "set_sprite_frame");
+
+    // Font and text functions
+    analyzer_declare_global(analyzer, "load_font");
+    analyzer_declare_global(analyzer, "default_font");
+    analyzer_declare_global(analyzer, "draw_text");
+    analyzer_declare_global(analyzer, "text_width");
+    analyzer_declare_global(analyzer, "text_height");
+
+    // Color constants
+    analyzer_declare_global(analyzer, "RED");
+    analyzer_declare_global(analyzer, "GREEN");
+    analyzer_declare_global(analyzer, "BLUE");
+    analyzer_declare_global(analyzer, "WHITE");
+    analyzer_declare_global(analyzer, "BLACK");
+    analyzer_declare_global(analyzer, "YELLOW");
+    analyzer_declare_global(analyzer, "CYAN");
+    analyzer_declare_global(analyzer, "MAGENTA");
+    analyzer_declare_global(analyzer, "ORANGE");
+    analyzer_declare_global(analyzer, "PURPLE");
+    analyzer_declare_global(analyzer, "GRAY");
+    analyzer_declare_global(analyzer, "GREY");
+
+    // Key constants
+    analyzer_declare_global(analyzer, "KEY_UP");
+    analyzer_declare_global(analyzer, "KEY_DOWN");
+    analyzer_declare_global(analyzer, "KEY_LEFT");
+    analyzer_declare_global(analyzer, "KEY_RIGHT");
+    analyzer_declare_global(analyzer, "KEY_SPACE");
+    analyzer_declare_global(analyzer, "KEY_RETURN");
+    analyzer_declare_global(analyzer, "KEY_ESCAPE");
+    analyzer_declare_global(analyzer, "KEY_TAB");
+    analyzer_declare_global(analyzer, "KEY_A");
+    analyzer_declare_global(analyzer, "KEY_B");
+    analyzer_declare_global(analyzer, "KEY_C");
+    analyzer_declare_global(analyzer, "KEY_D");
+    analyzer_declare_global(analyzer, "KEY_E");
+    analyzer_declare_global(analyzer, "KEY_F");
+    analyzer_declare_global(analyzer, "KEY_G");
+    analyzer_declare_global(analyzer, "KEY_H");
+    analyzer_declare_global(analyzer, "KEY_I");
+    analyzer_declare_global(analyzer, "KEY_J");
+    analyzer_declare_global(analyzer, "KEY_K");
+    analyzer_declare_global(analyzer, "KEY_L");
+    analyzer_declare_global(analyzer, "KEY_M");
+    analyzer_declare_global(analyzer, "KEY_N");
+    analyzer_declare_global(analyzer, "KEY_O");
+    analyzer_declare_global(analyzer, "KEY_P");
+    analyzer_declare_global(analyzer, "KEY_Q");
+    analyzer_declare_global(analyzer, "KEY_R");
+    analyzer_declare_global(analyzer, "KEY_S");
+    analyzer_declare_global(analyzer, "KEY_T");
+    analyzer_declare_global(analyzer, "KEY_U");
+    analyzer_declare_global(analyzer, "KEY_V");
+    analyzer
