69e2fa74ee3b3c02bae4014e4745ba7e77c307c6
Rani367 <rani2011367@gmail.com>
2025-08-19 11:08:00 +0300

    Implement base structure

--- src/pal/pal.c
+++ src/pal/pal.c
@@ -243,3 +243,286 @@ void pal_text_size(PalFont* font, const char* text, int* width, int* height) {
             break;
     }
 }
+          break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_draw_circle_outline(window, cx, cy, radius, r, g, b, a);
+            break;
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Textures
+// -----------------------------------------------------------------------------
+
+PalTexture* pal_texture_load(PalWindow* window, const char* path) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_texture_load(window, path);
+#else
+            return NULL;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_texture_load(window, path);
+    }
+    return NULL;
+}
+
+void pal_texture_destroy(PalTexture* texture) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_texture_destroy(texture);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_texture_destroy(texture);
+            break;
+    }
+}
+
+void pal_texture_get_size(PalTexture* texture, int* width, int* height) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_texture_get_size(texture, width, height);
+#else
+            if (width) *width = 0;
+            if (height) *height = 0;
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_texture_get_size(texture, width, height);
+            break;
+    }
+}
+
+void pal_draw_texture(PalWindow* window, PalTexture* texture,
+                      int x, int y, int width, int height) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_draw_texture(window, texture, x, y, width, height);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_draw_texture(window, texture, x, y, width, height);
+            break;
+    }
+}
+
+void pal_draw_texture_ex(PalWindow* window, PalTexture* texture,
+                         int x, int y, int width, int height,
+                         double rotation, int origin_x, int origin_y,
+                         bool flip_h, bool flip_v) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_draw_texture_ex(window, texture, x, y, width, height,
+                                    rotation, origin_x, origin_y, flip_h, flip_v);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_draw_texture_ex(window, texture, x, y, width, height,
+                                     rotation, origin_x, origin_y, flip_h, flip_v);
+            break;
+    }
+}
+
+void pal_draw_texture_region(PalWindow* window, PalTexture* texture,
+                             int src_x, int src_y, int src_w, int src_h,
+                             int dst_x, int dst_y, int dst_w, int dst_h) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_draw_texture_region(window, texture, src_x, src_y, src_w, src_h,
+                                        dst_x, dst_y, dst_w, dst_h);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_draw_texture_region(window, texture, src_x, src_y, src_w, src_h,
+                                         dst_x, dst_y, dst_w, dst_h);
+            break;
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Input - Keyboard
+// -----------------------------------------------------------------------------
+
+void pal_poll_events(void) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_poll_events();
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_poll_events();
+            break;
+    }
+}
+
+bool pal_should_quit(void) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_should_quit();
+#else
+            return true;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_should_quit();
+    }
+    return true;
+}
+
+bool pal_key_down(PalKey key) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_key_down(key);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_key_down(key);
+    }
+    return false;
+}
+
+bool pal_key_pressed(PalKey key) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_key_pressed(key);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_key_pressed(key);
+    }
+    return false;
+}
+
+bool pal_key_released(PalKey key) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_key_released(key);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_key_released(key);
+    }
+    return false;
+}
+
+// -----------------------------------------------------------------------------
+// Input - Mouse
+// -----------------------------------------------------------------------------
+
+void pal_mouse_position(int* x, int* y) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_mouse_position(x, y);
+#else
+            if (x) *x = 0;
+            if (y) *y = 0;
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_mouse_position(x, y);
+            break;
+    }
+}
+
+bool pal_mouse_down(PalMouseButton button) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_mouse_down(button);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_mouse_down(button);
+    }
+    return false;
+}
+
+bool pal_mouse_pressed(PalMouseButton button) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_mouse_pressed(button);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_mouse_pressed(button);
+    }
+    return false;
+}
+
+bool pal_mouse_released(PalMouseButton button) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_mouse_released(button);
+#else
+            return false;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_mouse_released(button);
+    }
+    return false;
+}
+
+// -----------------------------------------------------------------------------
+// Audio - Sound effects
+// -----------------------------------------------------------------------------
+
+PalSound* pal_sound_load(const char* path) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            return pal_sdl_sound_load(path);
+#else
+            return NULL;
+#endif
+        case PAL_BACKEND_MOCK:
+            return pal_mock_sound_load(path);
+    }
+    return NULL;
+}
+
+void pal_sound_destroy(PalSound* sound) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_sound_destroy(sound);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_sound_destroy(sound);
+            break;
+    }
+}
+
+void pal_sound_play(PalSound* sound) {
+    switch (current_backend) {
+        case PAL_BACKEND_SDL2:
+#ifdef PAL_USE_SDL2
+            pal_sdl_sound_play(sound);
+#endif
+            break;
+        case PAL_BACKEND_MOCK:
+            pal_mock_sound_play(sound);
+            break;
+    }
+}
+
+void pal_sound_play
