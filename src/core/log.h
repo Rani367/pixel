ec31a728575cde9aa251d27868d95ca35f527df4
Rani367 <rani2011367@gmail.com>
2025-08-23 17:03:09 +0300

    Add string functions

--- src/core/log.h
+++ src/core/log.h
@@ -12,4 +12,27 @@ typedef enum {
 } LogLevel;
 
 // Set the minimum log level (messages below this level are ignored)
-void log_set_level(LogLevel lev
+void log_set_level(LogLevel level);
+
+// Set the output file for log messages (default: stderr)
+void log_set_output(FILE* out);
+
+// Internal logging function (use the macros instead)
+#if defined(__GNUC__) || defined(__clang__)
+__attribute__((format(printf, 4, 5)))
+#endif
+void log_write(LogLevel level, const char* file, int line, const char* fmt, ...);
+
+// Logging macros
+#ifdef NDEBUG
+    // In release builds, disable trace and debug logging
+    #define LOG_TRACE(...) ((void)0)
+    #define LOG_DEBUG(...) ((void)0)
+#else
+    #define LOG_TRACE(...) log_write(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+    #define LOG_DEBUG(...) log_write(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#endif
+
+#define LOG_INFO(...)  log_write(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...)  log_write(LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) log_write(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS_
