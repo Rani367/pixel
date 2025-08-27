caef8d2da68648b67310004764fbb6c7540fccd2
Rani367 <rani2011367@gmail.com>
2025-08-27 17:08:33 +0300

    Implement tokenizer

--- src/compiler/analyzer.h
+++ src/compiler/analyzer.h
@@ -16,4 +16,10 @@ int analyzer_error_count(Analyzer* analyzer);
 // Get an error by index
 Error* analyzer_get_error(Analyzer* analyzer, int index);
 
-// Print all erro
+// Print all errors
+void analyzer_print_errors(Analyzer* analyzer, FILE* out);
+
+// Declare a global variable (for native functions, etc.)
+void analyzer_declare_global(Analyzer* analyzer, const char* name);
+
+#endif // PH_ANALYZER_H
