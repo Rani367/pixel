b517e3f13db2d3261210d51f9598d53c51fc5fbf
Rani367 <rani2011367@gmail.com>
2025-09-07 15:03:36 +0300

    Add sound effects

--- src/core/common.h
+++ src/core/common.h
@@ -4,4 +4,26 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <st
+#include <stle (0)
+#endif
+
+// Static array length
+#define PH_ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+// Unused parameter marker
+#define PH_UNUSED(x) ((void)(x))
+
+// Min/max macros
+#define PH_MIN(a, b) ((a) < (b) ? (a) : (b))
+#define PH_MAX(a, b) ((a) > (b) ? (a) : (b))
+
+// Memory allocation wrappers (can be replaced for custom allocators)
+#define PH_ALLOC(size) malloc(size)
+#define PH_REALLOC(ptr, size) realloc(ptr, size)
+#define PH_FREE(ptr) free(ptr)
+
+// Grow capacity for dynamic arrays (factor of 2)
+#define PH_GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#endif // PH_COMMON_H
