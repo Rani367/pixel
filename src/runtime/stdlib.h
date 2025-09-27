8cd794c5924fbd9fc251b065726e3f7b52ec26c1
Rani367 <rani2011367@gmail.com>
2025-09-28 00:50:39 +0300

    Implement shapes

---
+++ src/runtime/stdlib.h
@@ -0,0 +1,6 @@
+id stdlib_init(VM* vm);
+
+// Helper to define a native function in the VM's globals
+void define_native(VM* vm, const char* name, NativeFn function, int arity);
+
+#endif // PH_STDLIB_H
