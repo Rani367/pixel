160c1f43b8d394d1394d59d036593f7590aa5a84
Rani367 <rani2011367@gmail.com>
2025-10-31 07:23:00 +0200

    Implement shapes

--- src/compiler/symbols.h
+++ src/compiler/symbols.h
@@ -5,3 +5,12 @@ Symbol* scope_lookup(Scope* scope, const char* name, int length);
 bool names_equal(const char* a, int a_len, const char* b, int b_len);
 
 #endif // PH_SYMBOLS_H
+ a scope
+void scope_init(Scope* scope, int depth, Scope* enclosing);
+
+// Free a scope's symbol array (does not free enclosing scopes)
+void scope_free(Scope* scope);
+
+// Add a symbol to a scope (returns pointer to the added symbol)
+Symbol* scope_add_symbol(Scope* scope, const char* name, int length,
+        
