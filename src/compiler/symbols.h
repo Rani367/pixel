ff1be5a1faa7db1eb9c3c9df0641fd56217a9995
Rani367 <rani2011367@gmail.com>
2025-11-20 20:23:43 +0200

    Implement code generator

--- src/compiler/symbols.h
+++ src/compiler/symbols.h
@@ -13,4 +13,22 @@ void scope_free(Scope* scope);
 
 // Add a symbol to a scope (returns pointer to the added symbol)
 Symbol* scope_add_symbol(Scope* scope, const char* name, int length,
-        
+        #ifndef PH_SYMBOLS_H
+#define PH_SYMBOLS_H
+
+#include "core/common.h"
+#include "compiler/token.h"
+
+// Symbol kinds
+typedef enum {
+    SYMBOL_LOCAL,       // Local variable in a function
+    SYMBOL_GLOBAL,      // Global variable
+    SYMBOL_FUNCTION,    // Function definition
+    SYMBOL_STRUCT,      // Struct definition
+    SYMBOL_PARAMETER,   // Function parameter
+} SymbolKind;
+
+// Symbol entry in the symbol table
+typedef struct Symbol {
+    const char* name;       // Pointer into source (not owned)
+    int length;             // N
