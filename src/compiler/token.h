c1b00b92f06a4f9b1b90d4c6b4793c3a59a96211
Rani367 <rani2011367@gmail.com>
2025-12-07 20:14:33 +0200

    Implement AST nodes

--- src/compiler/token.h
+++ src/compiler/token.h
@@ -28,4 +28,54 @@ typedef enum {
     TTOKEN_PLUS_EQUAL,      // +=
     TOKEN_MINUS_EQUAL,     // -=
     TOKEN_STAR_EQUAL,      // *=
-    
+    TOKEN_SLASH_EQUAL,     // /=
+    TOKEN_PLUS_PLUS,       // ++
+    TOKEN_MINUS_MINUS,     // --
+
+    // Literals
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+
+    // Keywords
+    TOKEN_AND,
+    TOKEN_ELSE,
+    TOKEN_FALSE,
+    TOKEN_FOR,
+    TOKEN_FUNCTION,
+    TOKEN_IF,
+    TOKEN_IN,
+    TOKEN_NOT,
+    TOKEN_NULL,
+    TOKEN_OR,
+    TOKEN_RETURN,
+    TOKEN_STRUCT,
+    TOKEN_THIS,
+    TOKEN_TRUE,
+    TOKEN_WHILE,
+    TOKEN_BREAK,
+    TOKEN_CONTINUE,
+
+    // Special
+    TOKEN_ERROR,           // Lexer error (message in lexeme)
+    TOKEN_EOF,
+
+    TOKEN_COUNT
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    const char* start;     // Pointer into source
+    int length;
+    int line;
+    int column;
+} Token;
+
+// Get the name of a token type for debugging
+const char* token_type_name(TokenType type);
+
+// Print a token for debugging
+void token_print(Token token);
+
+// Create a token
+Token token_
